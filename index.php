3df066ead01e07fcb809135405e76f0c7b1999a5
Zendarion <j2.payet@rt-iut.re>
2016-04-21 09:04:25 +0400

    Added files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fuck</title>
+		<link rel="shortcut icon" href="favi.gif">
+			<link rel="stylesheet" type="text/css" href="stylesaf.css">
+			<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+			<meta charset="UTF-8">
+</head>
+<body onload="document.getElementById('text1').select();">
+
+	<header>
+		<div id="header"><img src="banner.jpg"></div>
+ 	<span class="glyphicon glyphicon-star" aria-hidden="true"></span></header>
+<div id="main">
+	<div id="contain">
+		<div class="panel panel-default">
+ 			 <div class="panel-heading">Contain</div>
+  			<div class="panel-body">
+  			<h1>You know nothing Safwaane Patel</h1>
+  			<p>
+	<script>
+aleatoire=Math.floor(Math.random()*5)
+switch(aleatoire){
+case 0:document.write("Leroooooy Jenkiiins !" );break;
+case 1:document.write("I'm your Father" );break;
+case 2:document.write("A ring to rule them all" );break;
+case 3:document.write("Mais Jerôme, ça dépasse l'entendement" );break;
+case 4:document.write("It's a trap!" );break;
+}
+</script>
+ 	 </p>
+			<a href="saf.jpg"><img src="got.jpg" ></a>
+
+   			 <script>
+
+			<!--
+			//alert('Voici');
+			// affiche ces variables
+			var a = "123"; 
+			var b = "456";
+
+			document.write(a+b,"<br>");
+
+
+			var tab = {fr:'Bob',it:'Simon',es:'Gerard'};
+
+			// var choix = confirm('sur?');
+
+			window.document.title = "Noob";
+
+			str = 'Boules de noel';
+			document.write('Bob aime les ' + str);
+			// function fonction(){
+			// 	var chp = document.getElementById('champ');
+			// 	chp.value++;
+			// }
+
+
+			
+
+			-->
+		</script>
+ 			 </div>
+		</div>
+		
+		<form>
+<!-- 			<input id="champ" value="1"></input>
+			<input onclick=""></input> -->
+		</form>
+
+	
+	</div>
+	<div id="side">
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Side</div>
+				<div class="panel-body">
+
+		<iframe src="http://tunein.com/embed/player/s105876/" style="width:100%;height:100px;" scrolling="no" frameborder="no" autoplay="true">
+			
+		</iframe>
+
+
+				</div>		
+			</div>
+
+	</div>
+	</div>
+</body>
+</html>
